1e4bf7fceec484efba7d0a98851c61956a138bcf
yumiKKKK <121678395+yumiKKKK@users.noreply.github.com>
2024-09-18 02:56:08 +0900

    Title: 바이러스 검사, Time: 355ms, Memory: 122MB, Status: Passed - CodeTree

--- 240918/바이러스 검사/virus-detector.py
+++ 240918/바이러스 검사/virus-detector.py
@@ -7,8 +7,8 @@ for i in range(n):
     customer[i] -= leader
     cnt += 1
     if customer[i] > 0:
-        k = customer / employee
-        if customer % employee == 0:
+        k = customer[i] // employee
+        if customer[i] % employee == 0:
             cnt += k
         else:
             cnt = cnt + 1 + k
