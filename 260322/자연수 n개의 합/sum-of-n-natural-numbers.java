3003ecfd438a91f2760d02d260764a3e21e3ffb4
Yumi Kim <121678395+yumiKKKK@users.noreply.github.com>
2026-03-22 23:53:25 +0900

    Title: 자연수 n개의 합, Time: 153ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260322/자연수 n개의 합/sum-of-n-natural-numbers.java
+++ 260322/자연수 n개의 합/sum-of-n-natural-numbers.java
@@ -1,28 +1,32 @@
-// 1부터 N까지의 자연수의 합이 S 이하인 경우 중 가능한 N의 최댓값
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         long S = sc.nextLong();
 
-        // logic
         long answer = 0;
         long left = 1;
         long right = S;
 
-        while(left <= right) {
+        while (left <= right) {
             long mid = left + (right - left) / 2;
 
-            if ((mid * (mid + 1)) / 2 <= S) {
+            long sum; // overflow
+            if (mid % 2 == 0) {
+                sum = (mid / 2) * (mid + 1);
+            } else {
+                sum = mid * ((mid + 1) / 2);
+            }
+
+            if (sum <= S) {
                 answer = mid;
                 left = mid + 1;
-            }
-            else {
+            } else {
                 right = mid - 1;
             }
         }
 
-        // output
         System.out.println(answer);
     }
 }
